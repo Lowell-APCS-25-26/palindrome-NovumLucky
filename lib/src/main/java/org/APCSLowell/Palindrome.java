597aad1ddba0fb815ac443b5d6079de11fb66394
NovumLucky <acostalucky77@gmail.com>
2026-02-06 03:02:29 +0000

    yup

--- lib/src/main/java/org/APCSLowell/Palindrome.java
+++ lib/src/main/java/org/APCSLowell/Palindrome.java
@@ -2,11 +2,12 @@ package org.APCSLowell;
 
 public class Palindrome {
     public boolean palindrome(String s) {
-        return false;
+        return s.equals(reverse(s));
     }
     public String reverse(String s) {
-        String n = new String();
-        // your code here
+        String n = new String("");
+        for (int i = s.length()-1; i >= 0; i--)
+            n += s.charAt(i);
         return n;
     }
 }
